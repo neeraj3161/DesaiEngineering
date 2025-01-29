676073964216ce9101a952ceeb4795e7d6ae9764
rahul1387 <rahultripathi7447@gmail.com>
2025-01-29 10:19:37 +0530

    tapovan tools stage 1

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,22 @@ anime.timeline({ loop: false })
         easing: "easeOutExpo",
         delay: 1000
     });
+
+
+const animatedElements = document.querySelectorAll('.animate__animated');
+
+// Create an Intersection Observer
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // Add the animation class
+            const animation = entry.target.dataset.animation;
+            entry.target.classList.add(animation);
+            entry.target.style.opacity = 1; // Make the element visible
+            observer.unobserve(entry.target); // Stop observing once animated
+        }
+    });
+}, { threshold: 0.1 }); // Trigger when 10% of the element is visible
+
+// Osbserve each animated element
+animatedElements.forEach(element => observer.observe(element));
